2a791e867e3953941de4e15dd76863205e3fb466
577294 <577294@LTUS102125.cts.com>
2019-07-15 14:37:05 -0400

    Added Basic files

---
+++ InnovationDemoPart1/src/com/discover/innovation/Demo.java
@@ -0,0 +1,11 @@
+package com.discover.innovation;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Innovators");
+ 
+	}
+
+}
