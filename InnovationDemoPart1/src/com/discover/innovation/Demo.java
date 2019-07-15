dc2c88bd534513c8b0ce04b8d3cbf914d55decb7
577294 <577294@LTUS102125.cts.com>
2019-07-15 14:44:44 -0400

    This is for initail commit

--- InnovationDemoPart1/src/com/discover/innovation/Demo.java
+++ InnovationDemoPart1/src/com/discover/innovation/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Innovators");
+		System.out.println("This is for Initial Demo Part 1");
  
 	}
 
